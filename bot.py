c86579e7d8c6f965609e0129641ee6d3aeba42fc
Marcell Kovats <mbkovats@buffalo.edu>
2023-08-12 03:04:45 -0400

    only bot commands

--- bot.py
+++ bot.py
@@ -31,8 +31,8 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
         
-        print(f"{username} said: '{user_message}' ({channel})")
-        if user_message[0] == "/":
+        # print(f"{username} said: '{user_message}' ({channel})")
+        if user_message[0] == "/" and channel == "bot-commands":
             user_message = user_message[1:]
             await send_message(message, user_message)
     client.run(BOT_TOKEN)
