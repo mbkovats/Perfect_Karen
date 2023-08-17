7ade7b135ca387542d02d11772b750028593c87c
Marcell Kovats <mbkovats@buffalo.edu>
2023-08-17 01:59:33 -0400

    join

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-import bot
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import random
 
-if __name__ == '__main__':
-    bot.run_discord_bot()
+load_dotenv()
+BOT_TOKEN = os.getenv('TOKEN')
+YT_TOKEN = os.getenv('YT_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix = '.', intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is now running!')
+
+@client.command()
+async def coin(ctx):
+    if random.randint(0,100)/100 >= 0.50:
+        await ctx.send("`Heads`") 
+    else:
+        await ctx.send("`Tails`")
+
+@client.command()
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+client.run(BOT_TOKEN)
